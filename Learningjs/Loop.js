6ff0bb2e0d354af4a4b17f57c95e02e1e05beb86
Toshit Kesarwani <87761872+Toshit101@users.noreply.github.com>
2025-08-10 14:51:18 +0000

    ALL LOOPS

---
+++ Learningjs/Loop.js
@@ -0,0 +1,19 @@
+// for(let i=1;i<=10;i++)
+// {
+//     console.log("TOSHUU");
+// }
+// let i = 1;
+
+
+// while (i <= 10) {
+
+
+//     console.log('');
+//     i++;
+// }
+let i= 1;
+do {
+console.log('This is do while loop');
+i++
+}while(i<=5);// ';' is must after while("")
+
