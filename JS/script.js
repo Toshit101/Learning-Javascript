29d46a1ac0d59094d4e9f3584bd7d9348d288fdf
Toshit Kesarwani <87761872+Toshit101@users.noreply.github.com>
2026-03-30 17:41:43 +0530

    Practiced on 30/03/2026

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+// let a = 10;
+// a = 90;
+// var b =11;
+//  b =20;
+// const c =10;
+// //  c =140; will show error as const value cannot be changed unlike var and let
+// console.log(a,b,c);
+// var x =1;//global
+// {
+//     var x =20; //global as var did not respect brackets
+// }
+// console.log("The value of x is ",x);
+// let value = 100;// block scope //global
+// {
+//     let value = 100; 
+//     console.log("Inside",value);
+// }
+// console.log("Outside",value);
+// let z =10;
+// let c = z;
+// console.log(z,c);//reference
+// let a= [1,2,3];
+// let b =a;//it seems like it copied the value of a in b but it does not if i change something in b it will change the value of a also
+// b.pop();//change done in B reflects in a too
+// console.log("The value of a is :",a); 
+// console.log("The value of b is :",b); 
+// let selectedstudents = null;
+//null means not assigning a value
+//undefined means not given a value to a variable so its gives by default which is undefined
+// let a ;
+// let u1 = Symbol("u1");
+// let u2 = Symbol("u1");
+// In JS  we do dynamic typing --> because here we can change data type
+let a =12;
+a ="True";
+
+ 
